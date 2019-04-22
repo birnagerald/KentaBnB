ea329f9089b4290765f2d6ad9ecb97eb63f80872
39291696 <birnagerald.pro@gmail.com>
2019-04-22 05:38:22 +0200

    HomePage

---
+++ src/Controller/AdminDashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\Stats;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminDashboardController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function index(ObjectManager $manager, Stats $stats)
+    {
+        $mainStats  = $stats->getStats();
+        $bestAds    = $stats->getAdsStats('DESC');
+        $worstAds   = $stats->getAdsStats('ASC');
+
+
+        return $this->render('admin/dashboard/index.html.twig', [
+            'stats'     => $mainStats,
+            'bestAds'   => $bestAds,
+            'worstAds'  => $worstAds
+        ]);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function home()
+    public function home(AdRepository $adRepo, UserRepository $userRepo)
     {
-        return $this->render('home/home.html.twig');
+        return $this->render('home/home.html.twig', [
+            'ads' => $adRepo->findBestAds(3),
+            'users' => $userRepo->findBestUsers(2)
+        ]);
     }
 }
